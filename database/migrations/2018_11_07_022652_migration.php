359addd2c1ed3bd3cd353cd311bfcf16cb9d28bc
Your Git's Name <Your Email>
2018-11-21 22:09:46 +0700

    project

---
+++ database/migrations/2018_11_07_022652_migration.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Migration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('Tieu DE');
+            $table->string('Mo ta');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+}
